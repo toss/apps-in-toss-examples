1e026ae7bfc3e95900e7c4edb5d15cdfba5df591
Duchan <jo_duchan@icloud.com>
2025-08-28 16:50:00 +0900

    chore: with-app-login

--- with-app-login/app/granite.config.ts
+++ with-app-login/app/granite.config.ts
@@ -1,15 +1,18 @@
-import { env } from '@react-native-bedrock/plugin-env';
+import { env } from '@granite-js/plugin-env';
+import { router } from '@granite-js/plugin-router';
 import { appsInToss } from '@apps-in-toss/framework/plugins';
-import { defineConfig } from 'react-native-bedrock/config';
+import { defineConfig } from '@granite-js/react-native/config';
 
 export default defineConfig({
+  scheme: 'intoss',
   appName: 'with-app-login',
+
   plugins: [
     appsInToss({
       permissions: [],
       brand: {
         displayName: '앱 로그인 예제',
-        icon: 'https://static.toss.im/appsintoss/73/b1a2730f-7f95-430b-92be-5516479f4cec',
+        icon: 'https://static.toss.im/appsintoss/73/6e532508-d5a9-4018-85b9-4c80bb2f0073.png',
         primaryColor: '#3B70E3',
         bridgeColorMode: 'basic',
       },
@@ -17,5 +20,6 @@ export default defineConfig({
     env({
       SERVER_BASE_URL: 'http://localhost:4000', // 로컬 서버 주소를 입력해 주세요.
     }),
+    router(),
   ],
 });
