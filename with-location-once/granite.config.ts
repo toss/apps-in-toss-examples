ec6fb9941464f552825ed32eb686a65983f155f4
Duchan <jo_duchan@icloud.com>
2025-09-01 15:36:34 +0900

    chore: with-location-once

--- with-location-once/granite.config.ts
+++ with-location-once/granite.config.ts
@@ -1,7 +1,9 @@
 import { appsInToss } from '@apps-in-toss/framework/plugins';
-import { defineConfig } from 'react-native-bedrock/config';
+import { router } from '@granite-js/plugin-router';
+import { defineConfig } from '@granite-js/react-native/config';
 
 export default defineConfig({
+  scheme: 'intoss',
   appName: 'with-location-once',
   plugins: [
     appsInToss({
@@ -13,10 +15,11 @@ export default defineConfig({
       ],
       brand: {
         displayName: '현재 위치 정보 예제',
-        icon: 'https://static.toss.im/appsintoss/73/6512da01-084b-4a42-a5e3-b06569fc443b',
+        icon: 'https://static.toss.im/appsintoss/73/62958236-e21c-4e1c-aa59-5332ef6cf6df.png',
         primaryColor: '#3B70E3',
         bridgeColorMode: 'basic',
       },
     }),
+    router(),
   ],
 });
