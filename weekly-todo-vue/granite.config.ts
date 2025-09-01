f74b905180b978d5f16df231151fb6db82e64b78
Duchan <jo_duchan@icloud.com>
2025-09-01 16:18:13 +0900

    chore: weekly-todo-vue

--- weekly-todo-vue/granite.config.ts
+++ weekly-todo-vue/granite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   outdir: 'dist',
   brand: {
     displayName: '위클리 투두 - 뷰',
-    icon: 'https://static.toss.im/appsintoss/73/84501e18-9de6-4ee6-b2fa-0eb8278fd62d',
+    icon: 'https://static.toss.im/appsintoss/73/082d577f-fcd5-41e1-8d54-d2dca123faa1.png',
     primaryColor: '#3B70E3',
     bridgeColorMode: 'inverted',
   },
