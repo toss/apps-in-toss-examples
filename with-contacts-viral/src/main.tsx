eaa9e71082b46cfae85fb4e4ea70677e266a2676
line0 <line0@toss.im>
2025-12-17 12:57:18 +0900

    chore: update package dependencies to @toss/tds components

--- with-contacts-viral/src/main.tsx
+++ with-contacts-viral/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { TDSMobileAITProvider } from '@toss-design-system/mobile-ait';
+import { TDSMobileAITProvider } from '@toss/tds-mobile-ait';
 import { App } from '@/App.tsx';
 import '@/index.css';
 
